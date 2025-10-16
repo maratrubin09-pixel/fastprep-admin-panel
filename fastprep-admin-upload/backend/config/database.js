46484975444772ed43991718dbc36a82093301a4
maratrubin09-pixel <marat.rubin09@gmail.com>
2025-10-15 22:21:28 -0400

    Update database.js

--- fastprep-admin-upload/backend/config/database.js
+++ fastprep-admin-upload/backend/config/database.js
@@ -21,12 +21,8 @@ module.exports = {
     storage: './test.sqlite',
     logging: false
   },
-  production: {
-    username: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT || 5432,
+   production: {
+    use_env_variable: 'DATABASE_URL',
     dialect: 'postgres',
     logging: false,
     dialectOptions: {
