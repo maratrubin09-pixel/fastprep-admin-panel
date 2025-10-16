2caf7c57cc760b2cfb2d0a69e6f6cf888f3828b3
maratrubin09-pixel <marat.rubin09@gmail.com>
2025-10-15 22:39:15 -0400

    Update sequelize.js

--- fastprep-admin-upload/backend/models/sequelize.js
+++ fastprep-admin-upload/backend/models/sequelize.js
@@ -1,26 +1,26 @@
-const { Sequelize } = require('sequelize');
-const config = require('../config/database');
+let sequelize;
 
-const env = process.env.NODE_ENV || 'development';
-const dbConfig = config[env];
-
-const sequelize = new Sequelize(
-  dbConfig.database,
-  dbConfig.username,
-  dbConfig.password,
-  {
-    host: dbConfig.host,
-    port: dbConfig.port,
+if (dbConfig.use_env_variable && process.env[dbConfig.use_env_variable]) {
+  sequelize = new Sequelize(process.env[dbConfig.use_env_variable], {
     dialect: dbConfig.dialect,
     logging: dbConfig.logging,
     dialectOptions: dbConfig.dialectOptions || {},
-    pool: {
-      max: 5,
-      min: 0,
-      acquire: 30000,
-      idle: 10000
+    pool: { max: 5, min: 0, acquire: 30000, idle: 10000 }
+  });
+} else {
+  sequelize = new Sequelize(
+    dbConfig.database,
+    dbConfig.username,
+    dbConfig.password,
+    {
+      host: dbConfig.host,
+      port: dbConfig.port,
+      dialect: dbConfig.dialect,
+      logging: dbConfig.logging,
+      dialectOptions: dbConfig.dialectOptions || {},
+      pool: { max: 5, min: 0, acquire: 30000, idle: 10000 }
     }
-  }
-);
+  );
+}
 
 module.exports = sequelize;
